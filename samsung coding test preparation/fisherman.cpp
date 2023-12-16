0a88eeedeaf2056ac4ddb4ed75f9759fd82c0617
masud45827 <masud.cse2019@gmail.com>
2023-12-16 11:24:20 +0600

    update

--- samsung coding test preparation/fisherman.cpp
+++ samsung coding test preparation/fisherman.cpp
@@ -1,32 +1,178 @@
 // https://www.hackerrank.com/contests/target-samsung-13-nov19/challenges/fisherman/problem
+// https://www.hackerrank.com/contests/target-samsung-13-nov19/challenges/fisherman/problem
 #include<iostream>
 using namespace std;
-int g1, g2, g3, n, ans = 1e9, tot = 0, x, y, z;
-void dfs(int i, int sum, int c1, int c2, int c3) {
-	if (i == n + 1) {
-		if (c1 == x && c2 == y && c3 == z) {
-			ans = min(ans, sum);
-		}
-		return;
-	}
-	if (c1 < x) {
-		dfs(i + 1, sum + abs(i - g1)+1, c1 + 1, c2, c3);
-	}
-	if (c2 < y) {
-		dfs(i + 1, sum + abs(i - g2)+1, c1, c2 + 1, c3);
-	}
-	if (c3 < z) {
-		dfs(i + 1, sum + abs(i - g3)+1, c1, c2, c3 + 1);
-	}
-	dfs(i + 1, sum, c1, c2, c3);
+int g1, g2, g3, n, ans = 1e9, tot = 0, x, y, z, res = 0;
+int g[4], vis[68], in[4];
+void dfs(int d1, int d2, int d3) {
+  int res = 0, res1 = 0;
+  for (int i = 1; i <= n; i++) vis[i] = 0;
+  int t = g[d1];
+  while (t--) {
+    int index = in[d1];
+    int l = index, r = index;
+    while (l>0 && vis[l]) l--;
+    while (r <= n && vis[r]) r++;
+    if (l < 1) l = -1e5; if (r > n) r = 1e5;
+    if (index - l < r - index) {
+      res += (index - l + 1);
+      vis[l] = 1;
+    } else if (index - l > r - index) {
+      res += (r - index + 1);
+      vis[r] = 1;
+    } else {
+      if (d1 != 3) {
+        res += (index - l + 1);
+        vis[l] = 1;
+      } else {
+        res += (r - index + 1);
+        vis[r] = 1;
+      }
+    }
+  }
+  t = g[d2];
+  while (t--) {
+    int index = in[d2];
+    int l = index, r = index;
+    while (l > 0 && vis[l]) l--;
+    while (r <= n && vis[r]) r++;
+    if (l < 1) l = -1e5; if (r > n) r = 1e5;
+    if (index - l < r - index) {
+      res += (index - l + 1);
+      vis[l] = 1;
+    } else if (index - l > r - index) {
+      res += (r - index + 1);
+      vis[r] = 1;
+    } else {
+      if (d1 != 3) {
+        res += (index - l + 1);
+        vis[l] = 1;
+      } else {
+        res += (r - index + 1);
+        vis[r] = 1;
+      }
+    }
+  }
+  t = g[d3];
+  while (t--) {
+    int index = in[d3];
+    int l = index, r = index;
+    while (l > 0 && vis[l]) l--;
+    while (r <= n && vis[r]) r++;
+    if (l < 1) l = -1e5; if (r > n) r = 1e5;
+    if (index - l < r - index) {
+      res += (index - l + 1);
+      vis[l] = 1;
+    } else if (index - l > r - index) {
+      res += (r - index + 1);
+      vis[r] = 1;
+    } else {
+      if (d1 != 3) {
+        res += (index - l + 1);
+        vis[l] = 1;
+      } else {
+        res += (r - index + 1);
+        vis[r] = 1;
+      }
+    }
+  }
+  ans = min(ans, res);
+  res=0;
+
+   for(int i=1;i<=n;i++) vis[i]=0;
+  t = g[d1];
+  while (t--) {
+    int index = in[d1];
+    int l = index, r = index;
+    while (l > 0 && vis[l]) l--;
+    while (r <= n && vis[r]) r++;
+    if (l < 1) l = -1e5; if (r > n) r = 1e5;
+    if (index - l < r - index) {
+      res += (index - l + 1);
+      vis[l] = 1;
+    } else if (index - l > r - index) {
+      res += (r - index + 1);
+      vis[r] = 1;
+    } else {
+      if (d1 != 1) {
+        res += (r - index + 1);
+        vis[r] = 1;
+
+      } else {
+        res += (index - l + 1);
+        vis[l] = 1;
+      }
+    }
+  }
+  t = g[d2];
+  while (t--) {
+    int index = in[d2];
+    int l = index, r = index;
+    while (l > 0 && vis[l]) l--;
+    while (r <= n && vis[r]) r++;
+    if (l < 1) l = -1e5; if (r > n) r = 1e5;
+    if (index - l < r - index) {
+      res += (index - l + 1);
+      vis[l] = 1;
+    } else if (index - l > r - index) {
+      res += (r - index + 1);
+      vis[r] = 1;
+    } else {
+      if (d1 != 1) {
+        res += (r - index + 1);
+        vis[r] = 1;
+
+      } else {
+        res += (index - l + 1);
+        vis[l] = 1;
+      }
+    }
+  }
+  t = g[d3];
+  while (t--) {
+    int index = in[d3];
+    int l = index, r = index;
+    while (l > 0 && vis[l]) l--;
+    while (r <= n && vis[r]) r++;
+    if (l < 1) l = -1e5; if (r > n) r = 1e5;
+    if (index - l < r - index) {
+      res += (index - l + 1);
+      vis[l] = 1;
+    } else if (index - l > r - index) {
+      res += (r - index + 1);
+      vis[r] = 1;
+    } else {
+      if (d1 != 1) {
+        res += (r - index + 1);
+        vis[r] = 1;
+
+      } else {
+        res += (index - l + 1);
+        vis[l] = 1;
+      }
+    }
+  }
+  ans = min(ans, res);
+
+
+
 
 }
 int main() {
-	int i, j;
-	cin >> n;
-	cin >> g1 >> g2 >> g3;
-	cin >> x >> y >> z;
-	tot = x + y + z;
-	dfs(1, 0, 0, 0, 0);
-	cout << ans << endl;
+  int t=1;
+ // cin >> t;
+  while (t--) {
+    int i, j;
+    cin >> n;
+    ans=1e9;
+    cin>>in[1]>>in[2]>>in[3];
+    cin>>g[1]>>g[2]>>g[3];
+    dfs(1, 2, 3);
+    dfs(1, 3, 2);
+    dfs(2, 1, 3);
+    dfs(2, 3, 1);
+    dfs(3, 2, 1);
+    dfs(3, 1, 2);
+    cout << ans << endl;
+  }
 }
